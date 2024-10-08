afc85abb88733f889c7e10396bccb2300b325ea9
amirjahan1 <amirjahan454@gmail.com>
2024-10-08 22:01:09 +0330

    Feature::->add nestjs to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [WalletModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'yourpassword',
+      database: 'root',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    WalletModule, // Import WalletModule to load the Wallet entity
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
+import { Wallet } from './entities/wallet.entity';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Wallet])], // Ensure SequelizeModule is imported here
   controllers: [WalletController],
   providers: [WalletService],
-  exports: [WalletService], // export if needed
+  exports: [WalletService],
 })
 export class WalletModule {}
